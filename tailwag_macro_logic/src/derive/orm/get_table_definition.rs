9bec3899c3dfdf1540da2fa626e9de95c4847282
Nik <nwakg@pm.me>
2023-09-10 19:08:09 -0700

    Add OnceCell to GetTableDefinition implementation

--- tailwag_macro_logic/src/derive/orm/get_table_definition.rs
+++ tailwag_macro_logic/src/derive/orm/get_table_definition.rs
@@ -1,63 +1,9 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{Ident, TokenStream};
 use quote::{format_ident, quote};
 use syn::{Data, DeriveInput};
+use tailwag_utils::strings::to_screaming_snake_case;
 
 const TRAIT_NAME: &'static str = "GetTableDefinition";
-
-fn build_get_table_definition(input: &DeriveInput) -> TokenStream {
-    // We build the table definition, and use that to (re-)build the creation logic.
-    // Other derives use this same function, so it creates a single place to make changes
-    // if the logic on how tables are built changes.
-    // Additional changes to support any syntax changes must still be added here
-    let input_table_definition =
-        crate::util::database_table_definition::build_table_definition(input);
-
-    // Build columns
-    let table_columns = input_table_definition.columns.iter().map(|column| {
-        let column_name: &str = &column.column_name;
-        let column_type = match column.column_type {
-            tailwag_orm::database_definition::table_definition::DatabaseColumnType::Boolean=>quote!(tailwag::orm::database_definition::table_definition::DatabaseColumnType::Boolean),
-            tailwag_orm::database_definition::table_definition::DatabaseColumnType::Int=>quote!(tailwag::orm::database_definition::table_definition::DatabaseColumnType::Int),
-            tailwag_orm::database_definition::table_definition::DatabaseColumnType::Float=>quote!(tailwag::orm::database_definition::table_definition::DatabaseColumnType::Float),
-            tailwag_orm::database_definition::table_definition::DatabaseColumnType::String=>quote!(tailwag::orm::database_definition::table_definition::DatabaseColumnType::String),
-            tailwag_orm::database_definition::table_definition::DatabaseColumnType::Timestamp=>quote!(tailwag::orm::database_definition::table_definition::DatabaseColumnType::Timestamp),
-            tailwag_orm::database_definition::table_definition::DatabaseColumnType::Uuid=>quote!(tailwag::orm::database_definition::table_definition::DatabaseColumnType::Uuid),
-        };
-        let constraints = column.constraints.iter().map(|constraint| {
-            match *constraint.detail {
-                tailwag_orm::database_definition::table_definition::TableColumnConstraintDetail::NotNull => quote!(.non_null()),
-                tailwag_orm::database_definition::table_definition::TableColumnConstraintDetail::PrimaryKey(_) => quote!(.pk()),
-                tailwag_orm::database_definition::table_definition::TableColumnConstraintDetail::References(_) => todo!(),
-                tailwag_orm::database_definition::table_definition::TableColumnConstraintDetail::Unique(_) => todo!(),
-                tailwag_orm::database_definition::table_definition::TableColumnConstraintDetail::Null => quote!(),
-            }
-        });
-
-        quote!(
-            tailwag::orm::database_definition::table_definition::TableColumn::new(&#column_name, #column_type, Vec::new()).expect("Invalid column name")
-            #(#constraints)*
-        )
-    });
-
-    let table_name = input_table_definition.table_name.as_str();
-
-    // !! START OF QUOTE
-    let tokens = quote!(
-        fn get_table_definition() -> tailwag::orm::database_definition::table_definition::DatabaseTableDefinition {
-            let table_def =
-                tailwag::orm::database_definition::table_definition::DatabaseTableDefinition::new(&#table_name)
-                .expect("Table name is invalid")
-                #(.column(#table_columns))*
-            //     // #(.constraint(#table_constraints)*) // TODO - weak constriants support currently
-                ;
-
-            table_def.into()
-        }
-    );
-    // !! END OF QUOTE
-
-    tokens
-}
 
 pub fn derive_struct(input: &DeriveInput) -> TokenStream {
     let &DeriveInput {
@@ -72,23 +18,22 @@ pub fn derive_struct(input: &DeriveInput) -> TokenStream {
     match &data.fields {
         syn::Fields::Named(fields) => {
             let _field_names = fields.named.iter().map(|f| &f.ident);
-            /////////////////////////////////////////
-            //         Functions Exported          //
-            /////////////////////////////////////////
-            let functions: Vec<TokenStream> = vec![
-                // todo!("Add functions here")
-                build_get_table_definition(input),
-            ];
+            let mut struct_name_screaming_case: String =
+                to_screaming_snake_case(&ident.to_string());
+            struct_name_screaming_case.push_str("_TABLE_DEFINITION");
+            let once_cell_name: TokenStream = struct_name_screaming_case.parse().unwrap();
 
-            ////////////////////////////////////////
-            // The actual output is defined here. //
-            ////////////////////////////////////////
+            let functions: Vec<TokenStream> =
+                vec![build_get_table_definition(input, once_cell_name.clone())];
 
-            // TODO: Think about how to handle Generics, when they end up being needed.
             let parse_args_impl_tokens = quote!(
+                static #once_cell_name: std::cell::OnceCell<
+                    tailwag::orm::data_manager::DatabaseTableDefinition<#ident>
+                > = OnceCell::new();
                 impl tailwag::orm::data_manager::GetTableDefinition for #ident {
                     #(#functions)*
                 }
+
             );
 
             parse_args_impl_tokens.into()
@@ -96,4 +41,63 @@ pub fn derive_struct(input: &DeriveInput) -> TokenStream {
         syn::Fields::Unnamed(_) => unimplemented!("Unnamed fields not supported yet"),
         syn::Fields::Unit => unimplemented!("Unit fields not supported yet"),
     }
+}
+
+fn build_get_table_definition(
+    input: &DeriveInput,
+    once_cell_name: TokenStream,
+) -> TokenStream {
+    // We build the table definition, and use that to (re-)build the creation logic.
+    // Other derives use this same function, so it creates a single place to make changes
+    // if the logic on how tables are built changes.
+    // Additional changes to support any syntax changes must still be added here
+    let input_table_definition =
+        crate::util::database_table_definition::build_table_definition(input);
+
+    // Build columns
+    let table_columns = input_table_definition.columns.iter().map(|column| {
+        let column_name: &str = &column.column_name;
+        let column_type = match column.column_type {
+            tailwag_orm::data_definition::table::DatabaseColumnType::Boolean=>quote!(tailwag::orm::data_definition::table::DatabaseColumnType::Boolean),
+            tailwag_orm::data_definition::table::DatabaseColumnType::Int=>quote!(tailwag::orm::data_definition::table::DatabaseColumnType::Int),
+            tailwag_orm::data_definition::table::DatabaseColumnType::Float=>quote!(tailwag::orm::data_definition::table::DatabaseColumnType::Float),
+            tailwag_orm::data_definition::table::DatabaseColumnType::String=>quote!(tailwag::orm::data_definition::table::DatabaseColumnType::String),
+            tailwag_orm::data_definition::table::DatabaseColumnType::Timestamp=>quote!(tailwag::orm::data_definition::table::DatabaseColumnType::Timestamp),
+            tailwag_orm::data_definition::table::DatabaseColumnType::Uuid=>quote!(tailwag::orm::data_definition::table::DatabaseColumnType::Uuid),
+        };
+        let constraints = column.constraints.iter().map(|constraint| {
+            match *constraint.detail {
+                tailwag_orm::data_definition::table::TableColumnConstraintDetail::NotNull => quote!(.non_null()),
+                tailwag_orm::data_definition::table::TableColumnConstraintDetail::PrimaryKey(_) => quote!(.pk()),
+                tailwag_orm::data_definition::table::TableColumnConstraintDetail::References(_) => todo!(),
+                tailwag_orm::data_definition::table::TableColumnConstraintDetail::Unique(_) => todo!(),
+                tailwag_orm::data_definition::table::TableColumnConstraintDetail::Null => quote!(),
+            }
+        });
+
+        quote!(
+            tailwag::orm::data_definition::table::TableColumn::new(&#column_name, #column_type, Vec::new()).expect("Invalid column name")
+            #(#constraints)*
+        )
+    });
+
+    let table_name = input_table_definition.table_name.as_str();
+
+    // !! START OF QUOTE
+    let tokens = quote!(
+        fn get_table_definition() -> tailwag::orm::data_definition::table::DatabaseTableDefinition {
+            let table_def: PostgresDataProvider<Self> = #once_cell_name.get_or_init(|| {
+                tailwag::orm::data_definition::table::DatabaseTableDefinition::new(&#table_name)
+                    .expect("Table name is invalid")
+                    #(.column(#table_columns))*
+                    // #(.constraint(#table_constraints)*) // TODO - weak constriants support currently
+                ;
+            });
+
+            table_def.into()
+        }
+    );
+    // !! END OF QUOTE
+
+    tokens
 }
