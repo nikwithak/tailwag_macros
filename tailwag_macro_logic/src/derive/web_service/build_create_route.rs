c2a6ebc112649dc786a2f0d3f3baf30dd1f3abb2
Nik <nwakg@pm.me>
2023-09-17 21:34:36 -0700

    Add logic for build_create_route and build_list_get_route

---
+++ tailwag_macro_logic/src/derive/web_service/build_create_route.rs
@@ -0,0 +1,72 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::{Data, DeriveInput, Field, Ident};
+
+use crate::util::attribute_parsing::GetAttribute;
+
+pub fn derive_struct(input: &DeriveInput) -> TokenStream {
+    let &DeriveInput {
+        ident,
+        data,
+        ..
+    } = &input;
+    // Panic with error message if we get a non-struct
+    let Data::Struct(data) = data else { panic!("Only Structs are supported") };
+
+    match &data.fields {
+        syn::Fields::Named(fields) => {
+            let fields_filtered: Vec<&Field> = fields
+                .named
+                .iter()
+                .filter(|field| {
+                    // TODO: Abstract this for easier filtering
+                    field.get_attribute("request_ignore").is_some() // TODO: `request(ignore)`
+                        || field.ident.as_ref().expect("Unexpected unnamed field found").to_string()
+                            != "id" // Always include "id"
+                })
+                .collect();
+            let field_names_filtered: Vec<&Ident> = fields_filtered
+                .iter()
+                .map(|f| f.ident.as_ref().expect("Found missing ident for field: {}"))
+                .collect();
+
+            let parse_args_impl_tokens = quote!(
+                impl<'a> tailwag::web::traits::rest_api::BuildCreateRoute<'a> for #ident
+                {
+                    fn build_create_route() -> axum::Router {
+                        #[derive(serde::Deserialize)]
+                        pub struct Request {
+                            #(#fields_filtered),*
+                        }
+                        impl Into<#ident> for Request {
+                            fn into(self) -> #ident {
+                                #ident {
+                                    // TODO: Don't assume ID here
+                                    id: uuid::Uuid::new_v4(),
+                                    #(#field_names_filtered: self.#field_names_filtered),*
+                                    // ..Default::default() // Todo: Require default? Or how to handle non-magic fields?
+                                }
+                            }
+                        }
+
+                        pub async fn post_item(
+                            axum::extract::State(data_manager): axum::extract::State<tailwag::orm::data_manager::PostgresDataProvider<#ident>>,
+                            axum::extract::Json(request): axum::extract::Json<Request>,
+                        ) -> axum::extract::Json<#ident> {
+                            let item: #ident = request.into();
+                            data_manager.create(&item).await.expect("Unable to create object");
+                            axum::extract::Json(item)
+                        }
+
+                        axum::Router::new()
+                            .route("/", axum::routing::method_routing::post(post_item))
+                    }
+                }
+            );
+
+            parse_args_impl_tokens.into()
+        },
+        syn::Fields::Unnamed(_) => unimplemented!("Unnamed fields not supported yet"),
+        syn::Fields::Unit => unimplemented!("Unit fields not supported yet"),
+    }
+}
---
+++ tailwag_macro_logic/src/derive/web_service/build_list_get_route.rs
@@ -0,0 +1,41 @@
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote};
+use syn::{Data, DeriveInput, Field, FieldsNamed, Ident};
+
+use crate::util::{self, attribute_parsing::GetAttribute};
+
+pub fn derive_struct(input: &DeriveInput) -> TokenStream {
+    let &DeriveInput {
+        ident,
+        data,
+        ..
+    } = &input;
+    // Panic with error message if we get a non-struct
+    let Data::Struct(data) = data else { panic!("Only Structs are supported") };
+
+    match &data.fields {
+        syn::Fields::Named(fields) => {
+            let parse_args_impl_tokens = quote!(
+                impl tailwag::web::traits::rest_api::BuildListGetRoute for #ident
+                {
+                    fn build_list_get_route() -> axum::Router {
+                        pub async fn list_items(
+                            axum::extract::State(data_manager): axum::extract::State<tailwag::orm::data_manager::PostgresDataProvider<#ident>>
+                        ) -> axum::extract::Json<Vec<#ident>> {
+                            // TODO: Revisit this when authorization rules are in place.
+                            // TODO: Add filtering via query params
+                            axum::extract::Json(data_manager.all().execute().await.unwrap())
+                        }
+
+                        axum::Router::new()
+                            .route("/", axum::routing::method_routing::get(list_items))
+                    }
+                }
+            );
+
+            parse_args_impl_tokens.into()
+        },
+        syn::Fields::Unnamed(_) => unimplemented!("Unnamed fields not supported yet"),
+        syn::Fields::Unit => unimplemented!("Unit fields not supported yet"),
+    }
+}
