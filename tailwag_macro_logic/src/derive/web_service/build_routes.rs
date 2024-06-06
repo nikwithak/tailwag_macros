e89f3b015364d935527455aba0588d498ac59f42
Nik <65584296+nikwithak@users.noreply.github.com>
2024-06-06 00:00:35 -0700

    Add support for paths isntead of just idents in BuildRoutes macro.

--- tailwag_macro_logic/src/derive/web_service/build_routes.rs
+++ tailwag_macro_logic/src/derive/web_service/build_routes.rs
@@ -1,8 +1,46 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{punctuated::Punctuated, Data, DeriveInput, Field, Ident, Token};
+use syn::{
+    punctuated::Punctuated, Data, DeriveInput, Expr, ExprLit, ExprPath, Ident, Lit, Path, Token,
+};
 
 use crate::util::attribute_parsing::GetAttribute;
+
+/// Helper function for extracting the route paths from an attribute.
+///
+/// Matches the following patterns:
+///
+/// #[actions("/nested/path/to/", handler_fn)] // Creates a route at `/{item_name}/nested/path/to`
+/// #[actions(func_name)] // Creates a route at `/{item_name}/func_name`
+fn get_route_paths(expr: Expr) -> (String, Ident) {
+    // Extract the actual attributes
+    match expr {
+        Expr::Path(path) => {
+            let ident = path.path.get_ident().cloned().expect("Path is not a valid identifier");
+            (ident.to_string(), ident)
+        },
+        Expr::Tuple(tuple) => {
+            let mut items = tuple.elems.iter();
+            let Some(Expr::Lit(ExprLit {
+                lit: Lit::Str(path_lit),
+                ..
+            })) = items.next()
+            else {
+                todo!("Unable to parse route paths.");
+            };
+            let path = path_lit.value();
+            let Some(Expr::Path(ExprPath {
+                path: func_path,
+                ..
+            })) = items.next()
+            else {
+                todo!("No function name provided");
+            };
+            (path, func_path.get_ident().cloned().expect("Didn't get valid ident"))
+        },
+        _ => todo!(),
+    }
+}
 
 pub fn derive_struct(input: &DeriveInput) -> TokenStream {
     let &DeriveInput {
@@ -14,21 +52,33 @@ pub fn derive_struct(input: &DeriveInput) -> TokenStream {
     let Data::Struct(data) = data else {
         panic!("Only Structs are supported")
     };
-    let actions = input
-        .get_attribute("actions")
-        .map(|attr| attr.parse_args_with(Punctuated::<Ident, Token![,]>::parse_terminated).unwrap())
-        .map(|params| params.into_iter().collect::<Vec<_>>())
-        .unwrap_or(Vec::new());
-    // Is this really how the only way to inject tokens into quote! with surrounding quotes?
-    let action_paths = actions.iter().map(|s| s.to_string());
 
-    let views = input
-        .get_attribute("views")
-        .map(|attr| attr.parse_args_with(Punctuated::<Ident, Token![,]>::parse_terminated).unwrap())
-        .map(|params| params.into_iter().collect::<Vec<_>>())
-        .unwrap_or(Vec::new());
-    // Is this really how the only way to inject tokens into quote! with surrounding quotes?
-    let view_paths = views.iter().map(|s| s.to_string());
+    fn extract_routes_from_attribute(
+        input: &DeriveInput,
+        attr_name: &str,
+    ) -> (Vec<String>, Vec<Ident>) {
+        input
+            .get_attribute(attr_name)
+            .map(|attr| {
+                attr.parse_args_with(Punctuated::<Expr, Token![,]>::parse_terminated).unwrap()
+            })
+            .map(|params| {
+                params.into_iter().map(get_route_paths).fold(
+                    (Vec::new(), Vec::new()),
+                    |(mut paths, mut funcs), (path, func)| {
+                        paths.push(path);
+                        funcs.push(func);
+                        (paths, funcs)
+                    },
+                )
+            })
+            .unwrap_or((Vec::new(), Vec::new()))
+    }
+
+    let (action_paths, actions): (Vec<String>, Vec<Ident>) =
+        extract_routes_from_attribute(input, "actions");
+    let (views_paths, views): (Vec<String>, Vec<Ident>) =
+        extract_routes_from_attribute(input, "views");
 
     // Default CRUD routes. Can be overridden with #[get(func_name), get("/", func_name), get_list("func_name")]
     let get_list_route_tokens = input
@@ -62,7 +112,7 @@ pub fn derive_struct(input: &DeriveInput) -> TokenStream {
                             //     Ok(Some(item)) => IntoResponse::into(item),
                             //     Ok(None) => Response::not_found(),
                             //     Err(_) => tailwag::web::application::http::route::Response::bad_request(),
-                            // }
+                            //  }
                             }
                         }
             )
@@ -71,7 +121,7 @@ pub fn derive_struct(input: &DeriveInput) -> TokenStream {
 
     let post_create_route_tokens = input
         .get_attribute("post")
-        .map(|attr| attr.parse_args::<Ident>().unwrap())
+        .map(|attr| attr.parse_args::<Path>().unwrap())
         .map(|func_name| quote!(.post("/", #func_name)))
         // .map(|params| params.into_iter().collect::<Vec<_>>())
         // .and_then(|params|)
@@ -128,7 +178,7 @@ pub fn derive_struct(input: &DeriveInput) -> TokenStream {
                             #patch_edit_route_tokens
                             #delete_route_tokens
                             #(.post(#action_paths, #actions))*
-                            #(.get(#view_paths, #views))*
+                            #(.get(#views_paths, #views))*
                     }
                 }
             );
