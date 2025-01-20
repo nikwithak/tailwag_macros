64c3e3af2e910ed7aa3cbb9a5696f66eced17834
Nik Gilmore <nik@nikgilmore.com>
2025-01-20 15:20:18 -0800

    Combine all macro crates into one.

--- src/macro_logic/mod.rs
+++ src/macro_logic/mod.rs
@@ -1,4 +1,4 @@
 // mod database_table_definition;
 pub mod attribute;
 pub mod derive;
-mod util;
+pub mod util;
--- tailwag_macro_logic/src/derive/web_service/mod.rs
+++
@@ -1,6 +0,0 @@
-mod rest_api;
-pub use rest_api::*;
-pub mod build_create_route;
-pub mod build_list_get_route;
-pub mod build_routes;
-// pub use build_routes::*;
